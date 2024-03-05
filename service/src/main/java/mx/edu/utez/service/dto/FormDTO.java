ce1744229a0ae0f783ccef5290cc27a6dbf2ff5a
AntonioCarri11o <20203tn078@utez.edu.mx>
2024-03-05 00:09:00 -0600

    avance

--- service/src/main/java/mx/edu/utez/service/dto/FormDTO.java
+++ service/src/main/java/mx/edu/utez/service/dto/FormDTO.java
@@ -11,13 +11,15 @@ import mx.edu.utez.service.model.Form;
 @Getter @Setter
 public class FormDTO {
     @NotBlank(message = "Propiedad text requerida")
-    @Size(min = 10, max = 20, message = "El campo de texto debe tener enre 3 y 10 caracteres")
-    @Pattern(regexp = "\\S+", message = "El campo texto no puede contener solo espacios en blanco")
+    @Size(min = 10, max = 20, message = "El campo de texto debe tener entre 10 y 20 caracteres")
+    @Pattern(regexp = "^(?=.*\\S).+$", message = "El campo texto no puede contener solo espacios en blanco")
     private String text;
 
     @NotBlank(message = "Propiedad card_number requerida")
-    @Pattern(regexp = "^([0-9]{4}[0-9]{4}[0-9]{4}[0-9]{4})$^", message = "Número de tarjeta inválido")
+    @Pattern(regexp = "^[0-9]{16}$", message = "Número de tarjeta inválido")
     private String card_number;
+    @NotBlank(message = "Propiedad rfc requerida")
+    @Pattern(regexp = "^([A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}[0-1]{1}[0-9]{1}[0-3]{1}[0-9]{1})$", message = "Formato inválido pendejo")
     private String rfc;
 
 
